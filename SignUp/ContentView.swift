f0ae9593cbd118a7fa13498c50f4accb69640ae8
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-10-29 17:57:58 -0300

    feat: Implementing the skeleton of the signup page

--- SignUp/ContentView.swift
+++ SignUp/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State var username = ""
+	@State var password = ""
+
+	var body: some View {
+		ZStack {
+			Color.primaryTheme.ignoresSafeArea()
+			VStack(alignment: .leading, spacing: 24) {
+				Text("Sign Up")
+					.font(.system(size: 32, weight: .heavy))
+					.foregroundStyle(.white)
+				Text("Username")
+					.font(.system(size: 16, weight: .bold))
+					.foregroundStyle(.white)
+				TextField("Username", text: $username)
+					.textFieldStyle(.roundedBorder)
+					.textContentType(.username)
+					.textInputAutocapitalization(.never)
+					.autocorrectionDisabled()
+				Text("Email")
+					.font(.system(size: 16, weight: .bold))
+					.foregroundStyle(.white)
+				TextField("Email", text: $username)
+					.textFieldStyle(.roundedBorder)
+					.textContentType(.emailAddress)
+					.textInputAutocapitalization(.never)
+					.autocorrectionDisabled()
+				Text("Password")
+					.font(.system(size: 16, weight: .bold))
+					.foregroundStyle(.white)
+				SecureField("Password", text: $password)
+					.textFieldStyle(.roundedBorder)
+				Button(
+					action: {},
+					label: {
+						Text("Submit")
+							.foregroundStyle(.white)
+							.font(.system(size: 16, weight: .semibold))
+							.frame(maxWidth: .infinity)
+							.frame(height: 50)
+					}
+				)
+				.background(.black)
+				.clipShape(RoundedRectangle(cornerRadius: 8))
+				.padding(.top)
+				Spacer()
+			}
+			.padding(.horizontal)
+		}
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
