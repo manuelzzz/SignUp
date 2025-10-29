8abfe7967900c4a9a6495e9f17a30d930f2bdb53
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-10-29 15:51:14 -0300

    Adding the project

---
+++ SignUp/SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpApp.swift
+//  SignUp
+//
+//  Created by Manuel Santos Souza on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
